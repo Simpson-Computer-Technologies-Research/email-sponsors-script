36b35bdcacbca58d8886f86eab76bc59d70d69b5
tristan <heytristaann@gmail.com>
2024-02-23 11:51:47 -0500

    c

--- utils/email_format.py
+++ utils/email_format.py
@@ -21,9 +21,9 @@ Dear {contact_name},
 
 I hope this email finds you well. My name is {name} and I’m the {role} for the Google Developer Student Club (GDSC) at the University of Guelph. I am reaching out to express my interest in {company} partnering with the GDSC for our upcoming hackathon that will take place from May 3rd to May 5th. The Google Developer Student Club is a student-led organization supported by Google – dedicated to fostering an innovative environment for over 250 young tech professionals in Guelph.
 
-With your financial support, {company} will be able to contribute to a memorable experience for students. In return, you will gain exclusive access to emerging talent and student resumes, significant brand visibility (specific details can be found on our sponsorship package), and recognition for your commitment to education – strengthening your relationship with the University of Guelph. We’d be more than happy to provide our sponsorship package with all of the exclusive perks and marketing opportunities that we provide.
+With your support, {company} will be able to contribute to a memorable experience for students. In return, you will gain exclusive access to emerging talent and student resumes, significant brand visibility (specific details can be found on our sponsorship package), and recognition for your commitment to education – strengthening your relationship with the University of Guelph. We’d be more than happy to provide our sponsorship package with all of the exclusive perks and marketing opportunities that we provide.
 
-We are confident that a partnership between the GDSC and {company} will yield substantial mutual benefits. We appreciate your consideration of this partnership opportunity Please feel free to reach out if you have any questions or if there is any other information that I can provide.
+We appreciate your consideration, and of course feel free to reach out if you have any questions or if there is any other information that I can provide.
 
 Thank you,
 
